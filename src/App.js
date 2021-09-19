92ebc06daccfbcf4b72f01bb4cf57e77318bdd4b
Anirudh Jwala <anirudhsai2@gmail.com>
2021-09-19 14:52:43 +0530

    Sign in page with sign-in component & form-input custom styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
+// Components
 import Header from './components/header/header.component';
 
+// Pages
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+// Styles
 import './App.css';
 
 function App() {
@@ -14,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signIn" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
